fbf6cbe0e84f87350b643a4569e6db8339b655d9
Charlene Moore <charlenemoore86@gmail.com>
2018-12-13 15:37:54 +0000

    Final Project Code.R

---
+++ Final Project Code.R
@@ -0,0 +1,12 @@
+# Charlene Moore
+# x15412048
+# Final Project Code
+# Student at National College of Ireland
+# Lecture: Eaomon Nolan
+# Supervisor: Dr. Eugene O'Loughlin
+#
+# Analysis on Gun crime in America
+#
+# Reading in main dataset on gun-violence-data downloaded from Kaggle.com who scraped it from GunViolenceData(GVA)
+
+
