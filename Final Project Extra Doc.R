b0fcc818d3b86945c34c689edc6e3cedbe1d6c33
Charlene Moore <charlenemoore86@gmail.com>
2018-12-13 15:38:48 +0000

    Final Project Extra Doc.R

---
+++ Final Project Extra Doc.R
@@ -0,0 +1,6 @@
+# Final Project Functions and packages 
+#
+install.packages("DMwR2") # Functions and data for the second edition of the book "Data Mining with R"
+#
+old.packages()
+update.packages()
